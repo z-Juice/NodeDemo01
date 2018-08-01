3c892c1f6d9de2c90512ecffbae47e85c451f0fa
zhangzy <515357583@qq.com>
2018-08-01 14:49:20 +0800

    修改

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const xtpl = require('xtpl');
 const bodyParser = require('body-parser');
 const multer = require('multer');
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 
 let app = express();
 
